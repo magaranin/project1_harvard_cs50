a10372552c4b92cf665c10adf04c3dab89341553
Margarita Sirbu <margaritapetru@hotmail.com>
2020-10-25 22:46:15 -0700

    Add page 'crete_new_page.html' that creates a new wiki entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>",views.entry_page, name="entry_page")
+    path("wiki/<str:title>",views.entry_page, name="entry_page"),
+    path("create_new_entry", views.create_new_entry, name="create_new_entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from . import util
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 import markdown2
+from django import forms
+from django.urls import reverse
+
+class new_entry_form(forms.Form):
+    entry = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control col-md-8 col-lg-8'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control col-md-8 col-lg-10', 'rows' : 10}))
+
     
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -14,4 +22,20 @@ def entry_page(request, title):
     return render(request, "encyclopedia/entry_page.html", {
         "content": file_content
     })
+
+def create_new_entry(request):
+    if request.method == "POST":
+        form = new_entry_form(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["entry"]
+            content = form.cleaned_data["content"]
+            if title in util.list_entries():
+                return HttpResponseNotFound('<h1>This entry already exists</h1>')               
+            else:
+                util.save_entry(title,content)
+                return HttpResponseRedirect(reverse("entry_page", kwargs={'title': title}))
+    return render(request, "encyclopedia/create_new_entry.html", {
+        "form": new_entry_form()
+    })
+
 
