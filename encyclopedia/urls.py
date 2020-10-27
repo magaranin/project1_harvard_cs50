4579e6a1c138a7d416df34c8699e1fe1d601f506
Margarita Sirbu <margaritapetru@hotmail.com>
2020-10-27 00:05:46 -0700

    Add error_page.html and functionality to the search box

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>",views.entry_page, name="entry_page"),
-    path("create_new_entry", views.create_new_entry, name="create_new_entry")
+    path("create_new_entry", views.create_new_entry, name="create_new_entry"),
+    path("search", views.search, name="search"),
+    path("error_page", views.error_page, name="error_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . import util
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponseRedirect
 import markdown2
 from django import forms
 from django.urls import reverse
@@ -8,7 +8,6 @@ from django.urls import reverse
 class new_entry_form(forms.Form):
     entry = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control col-md-8 col-lg-8'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control col-md-8 col-lg-10', 'rows' : 10}))
-
     
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -29,13 +28,33 @@ def create_new_entry(request):
         if form.is_valid():
             title = form.cleaned_data["entry"]
             content = form.cleaned_data["content"]
-            if title in util.list_entries():
-                return HttpResponseNotFound('<h1>This entry already exists</h1>')               
+            if title.upper() in [element.upper() for element in util.list_entries()]:
+                return error_page(request, "This entry already exists!")              
             else:
                 util.save_entry(title,content)
                 return HttpResponseRedirect(reverse("entry_page", kwargs={'title': title}))
     return render(request, "encyclopedia/create_new_entry.html", {
         "form": new_entry_form()
+    })
+
+def search(request):
+    value = request.GET['q']
+    if util.get_entry(value): #request=title
+        return HttpResponseRedirect(reverse("entry_page", kwargs={'title': value}))
+    else: #request !=title
+        selected_values = []
+        for entry in util.list_entries():
+            if value.upper() in entry.upper():
+                selected_values.append(entry)
+        if len(selected_values)==0:
+            return error_page(request, "No entry has been found!")
+    return render(request, "encyclopedia/index.html", {
+        "entries": selected_values
+    })
+        
+def error_page(request, error):
+    return render(request, "encyclopedia/error_page.html", {
+        'error': error
     })
 
 
